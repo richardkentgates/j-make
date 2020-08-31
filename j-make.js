dcb480aa876988f115aaebe742c1df2411ec03b6
Richard Gates <mail@richardkentgates.com>
2020-08-31 12:09:26 -0500

    Fixed recursive bug and added dataset to carry directory path

--- j-make.js
+++ j-make.js
@@ -1,10 +1,22 @@
 (function() {
     jMake = {};
     jMake.functions = {};
-    jMake.functions.make = function(p,k){
+    jMake.functions.make = function(p){
+        console.log(p);
         $.get(p, function(data, status){
-            $(data).prependTo( "#" + k );
+            $(data).prependTo('*[data-path="' + p + '"]');
         });
+    };
+    jMake.functions.nodeTreeToArray = function(e){
+        jMake.nodeTreeArray = Array();
+        while(e=e.parentNode){
+                if(!e.dataset.key){
+                    break;
+                } else {
+                    jMake.nodeTreeArray.push(e.dataset.key);
+                }
+        }
+        return jMake.nodeTreeArray;
     };
     jMake.functions.matchNode = function(e){
         jMake.nodeIndex= 0;
@@ -12,27 +24,24 @@
         return jMake.nodeIndex;
     };
     jMake.functions.strings = function(v,e){
-        jMake.elements = document.createElement(v);
-        e.appendChild(jMake.elements);
-        jMake.key = v + '_' + jMake.functions.matchNode(jMake.elements);
-        jMake.elements.id = jMake.key;
-        jMake.elements.className = 'j-make';
-        jMake.parentPathArray = $(jMake.elements).parents().map(function(i,mappedItem){
-            if(mappedItem.id.length > 0){
-              return mappedItem.id;
-            } else {
-                if(mappedItem.tagName.toLowerCase() !== 'html'){
-                  return mappedItem.tagName;
-                }
-            }
-        })
-        .get();
-        jMake.path = jMake.parentPathArray.reverse().join("/").toLowerCase() + '/' + jMake.key;
-        jMake.functions.make(jMake.path,jMake.key);
+        jMake.element = document.createElement(v);
+        e.appendChild(jMake.element);
+        jMake.nodeTreeToArray = jMake.functions.nodeTreeToArray(jMake.element).reverse();
+        jMake.matchNode = jMake.functions.matchNode(jMake.element);
+        jMake.key = v + '_' + jMake.matchNode;
+        jMake.element.dataset.key = jMake.key;
+        jMake.element.className = 'j-make';
+        if(jMake.nodeTreeToArray.length > 0){
+            jMake.body = 'body/';
+        } else {
+            jMake.body = 'body';
+        }
+        jMake.element.dataset.path = jMake.body + jMake.nodeTreeToArray.join('/') + '/' + jMake.key;
+        jMake.functions.make(jMake.element.dataset.path);
     };
     jMake.functions.arrays = function(v){
         if(jQuery.type(v) == 'array'){
-            jMake.functions.j(v,jMake.elements);
+            jMake.functions.j(v,jMake.element);
         } else {
             alert('Invalid value: ' + jType + ' type');
         }
